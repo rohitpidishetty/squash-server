56a8692a800e0d1e54470985a95e6d0ddc0bbaab
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-01-12 16:57:57 -0600

    Update SecurityConfig.java

--- src/main/java/com/squash/squash/Config/SecurityConfig.java
+++ src/main/java/com/squash/squash/Config/SecurityConfig.java
@@ -3,12 +3,10 @@ package com.squash.squash.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,8 +16,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 
+
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -58,6 +56,7 @@ public class SecurityConfig {
         return source;
     }
 }
+
 
 
 
